9807a3db8f047bd0ad9bddee241ce58d4d335c8b
UriMakarov <umakarov@colvir.com>
2024-07-02 16:52:45 +0300

    вынес обращение к БД таблицы employees в сервисный слой

--- src/main/java/com/colvir/simpleJPA/controller/EmployeeController.java
+++ src/main/java/com/colvir/simpleJPA/controller/EmployeeController.java
@@ -2,7 +2,7 @@ package com.colvir.simpleJPA.controller;
 
 
 import com.colvir.simpleJPA.model.Employee;
-import com.colvir.simpleJPA.repository.EmployeeRepository;
+import com.colvir.simpleJPA.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,44 +15,35 @@ import java.util.List;
 public class EmployeeController {
 
     @Autowired
-    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
 
     @GetMapping
     public List<Employee> getAll(){
-       return employeeRepository.findAll();
+       return employeeService.getAllEmployees();
     }
     @GetMapping("/{id}")
     public Employee getById(@PathVariable Integer id){
-        return employeeRepository.findById(id)
+        return employeeService.getEmployeeById(id)
                 .orElseThrow(() -> new RuntimeException("Отсутствует сотрудник с ID: " + id));
     }
 
     @PostMapping
     public Employee create(@RequestBody Employee employee){
-        return employeeRepository.save(employee);
+        return employeeService.createEmployee(employee);
     }
-
 
     @PutMapping("/{id}")
     public Employee update(@PathVariable Integer id, @RequestBody Employee employeeForUpdate){
-        Employee employee =  employeeRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Отсутствует сотрудник с ID: " + id));
-
-        employee.setFirstName(employeeForUpdate.getFirstName());
-        employee.setLastName(employeeForUpdate.getLastName());
-        employee.setDepartment(employeeForUpdate.getDepartment());
-        employee.setSalary(employeeForUpdate.getSalary());
-
-        return employeeRepository.save(employee);
+        return employeeService.updateEmployee(employeeForUpdate);
     }
 
 
     @DeleteMapping("/{id}")
     public String delete(@PathVariable Integer id) {
-        Employee employee = employeeRepository.findById(id)
+        Employee employee = employeeService.getEmployeeById(id)
                 .orElseThrow(() -> new RuntimeException("Отсутствует сотрудник с ID: " + id));
 
-        employeeRepository.delete(employee);
+        employeeService.deleteEmployee(id);
         return "Удалён сотрудник с ID: " + id;
     }
 }
